11beb91946dc9f4a18fe396fbe7dfbbacab9264a
jeanevitoria <131841615+jeanevitoria@users.noreply.github.com>
2023-09-11 19:48:51 -0300

    Delete Redes_Eletricas_Com_Gafos1.1 directory

--- Redes_Eletricas_Com_Gafos1.1/src/exception/OperacaoInvalidaException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class OperacaoInvalidaException extends RuntimeException {
-	private static final long serialVersionUID = -994753840728974048L;
-
-	public OperacaoInvalidaException(String message) {
-		super(message);
-	}
-}
